4c95c89380f0ff6b1990f723d0521474860b1f48
Nathan Hanson <120321153+NatethisisGit@users.noreply.github.com>
2023-06-17 22:43:09 -0400

    Update app.py
    small change

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return 'Please subscribe, like, and comment on this video, TY!!! And if you dont that will not be cool '
